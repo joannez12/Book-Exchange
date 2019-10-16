4d55236cd7a4b6e16f94e38342f50261fb652550
Eric Wong <ewong211@outlook.com>
2019-10-16 15:44:02 -0400

    Initialize components for search/browse page

---
+++ bkexch-react/src/components/TextbookList/TextbookList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TextbookList() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default TextbookList;
---
+++ bkexch-react/src/components/TextbookListing/TextbookListing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TextbookListing() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default TextbookListing;
