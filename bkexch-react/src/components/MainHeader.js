4eb816b2123a68cdba0a1a75d76c9fd07cfc1c64
joannez12 <55938037+joannez12@users.noreply.github.com>
2019-10-24 15:13:45 -0400

    Add files via upload

--- bkexch-react/src/components/MainHeader.js
+++ bkexch-react/src/components/MainHeader.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import SignupPopup from '../SignupPopup';
 
 class MainHeader extends React.Component {
+	state = {
+		signup: false
+	}
+
+	handleSignup = () => {
+		this.setState({signup: !this.state.signup})
+	}
+
     render(){
         return(
+        	<>
             <div className='mainheader'>
                 <h3><a href="/">Home</a></h3>
-                <h3><a href="/signup">Sign Up</a></h3>
+                <h3><a onClick = { () => this.handleSignup() }>Sign Up</a></h3>
+                
                 <h3><a href="/history">History</a></h3>
+             
+             
             </div>
+             { this.state.signup ? <SignupPopup close = { () => this.handleSignup() } /> : null }
+           	</>
         )
     }
 }
