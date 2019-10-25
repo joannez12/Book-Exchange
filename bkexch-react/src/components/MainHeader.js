fa8ef0f22f6aa7a1393d765c84607e19fb540d8c
yunsangr <sangryul.yun@gmail.com>
2019-10-24 23:34:23 -0400

    initalize postpopup view & small change in mainheader

--- bkexch-react/src/components/MainHeader.js
+++ bkexch-react/src/components/MainHeader.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import SignupPopup from '../SignupPopup';
-
+import PostPopUp from "../pages/PostPopUp/PostPopUp";
 class MainHeader extends React.Component {
 	state = {
-		signup: false
+		signup: false,
+        addPost: false
 	}
 
 	handleSignup = () => {
 		this.setState({signup: !this.state.signup})
 	}
+    handlePostPopUp = () => {
+        this.setState({addPost: !this.state.addPost, signup: false},
+        )
+    }
 
     render(){
         return(
@@ -16,12 +21,14 @@ class MainHeader extends React.Component {
             <div className='mainheader'>
                 <h3><a href="/">Home</a></h3>
                 <h3><a onClick = { () => this.handleSignup() }>Sign Up</a></h3>
-                
+                <h3><a onClick = { () => this.handlePostPopUp() } >Post</a></h3>
                 <h3><a href="/history">History</a></h3>
-             
-             
+
             </div>
              { this.state.signup ? <SignupPopup close = { () => this.handleSignup() } /> : null }
+                {
+                    this.state.addPost ? <PostPopUp /> : null
+                }
            	</>
         )
     }
---
+++ bkexch-react/src/pages/PostPopUp/PostPopUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../SignupPopup.css';
+
+
+class PostPopUp extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <form className="popupContent">
+                <h3> NEW POST !! </h3>
+            </form>
+        )
+    }
+}
+
+export default PostPopUp;
