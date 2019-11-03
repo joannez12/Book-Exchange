66c362f3134d89c9c7f785f1845b6fc8c551df1d
yunsangr <sangryul.yun@gmail.com>
2019-11-03 00:03:14 -0400

    initialize messagebox component

--- bkexch-react/src/App.js
+++ bkexch-react/src/App.js
@@ -3,6 +3,7 @@ import SearchBrowse from './pages/SearchBrowse/SearchBrowse';
 import HistoryBrowse from "./pages/HistoryBrowse/HistoryBrowse";
 import MainHeader from './components/Navbar/MainHeader';
 import ViewTextbook from './pages/ViewTextbook/ViewTextbook';
+import MessageBox from "./pages/MessageBox/MessageBox";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -23,6 +24,7 @@ class App extends React.Component {
           <MainHeader user={this.state.user} handleSignin={this.handleSignin} deleted={()=>this.setState({user: null})} />
           <Switch>
             <Route exact path="/" component={SearchBrowse} />
+            {this.state.user ? <Route exact path="/messagebox" component={ () => <MessageBox />} /> : null }
             {this.state.user ? <Route exact path="/history" component={ () => <HistoryBrowse user={this.state.user} />} /> : null }
             <Route path="/textbooks/:id" children={<ViewTextbook/>} />
           </Switch>
---
+++ bkexch-react/src/pages/MessageBox/MessageBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+class MessageBox extends React.Component{
+
+    render(){
+        return(
+            <div className='Messagebox'>
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+                MESSAGE BOX!!
+            </div>
+        )
+    }
+}
+
+
+export default MessageBox;
