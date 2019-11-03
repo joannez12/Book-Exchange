9fd287a7a307df922fb10bc29a5db26602547b94
yunsangr <sangryul.yun@gmail.com>
2019-11-03 03:59:07 -0500

    adding contact link to textbooklisting

--- bkexch-react/src/components/TextbookListing/TextbookListing.js
+++ bkexch-react/src/components/TextbookListing/TextbookListing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { useHistory} from 'react-router-dom';
 import './TextbookListing.css';
+import {Col, Row} from "react-bootstrap";
 
 function TextbookListing({ textbook }) {
     const { id, title, author, seller, price} = textbook;
@@ -12,7 +13,12 @@ function TextbookListing({ textbook }) {
                     <Card.Title>{title}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{author}</Card.Subtitle>
                 </Card.Body>
-                <Card.Footer className="text-muted"><small>${price} - {seller}</small></Card.Footer>
+                <Card.Footer className="text-muted">
+                    <Row>
+                        <Col xs={8}><small>${price} - {seller}</small></Col>
+                        <Col><Card.Link href="#">Contact</Card.Link></Col>
+                    </Row>
+                </Card.Footer>
             </Card>
     )
 }
