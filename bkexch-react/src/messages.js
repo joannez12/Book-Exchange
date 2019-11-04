153cfa10e3eb30d1204d62f2a3692d1e6fc65f38
yunsangr <sangryul.yun@gmail.com>
2019-11-03 19:02:22 -0500

    deleteMessage function added

--- bkexch-react/src/messages.js
+++ bkexch-react/src/messages.js
@@ -4,7 +4,6 @@ const messages = [
 export const deleteMessage = (message) => {
     const index = messages.indexOf(message);
     if (index > -1) {
-        console.log("here");
         messages.splice(index, 1);
     }
 }
