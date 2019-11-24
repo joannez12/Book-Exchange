ee05dee278599c595c1460ac592f44021367f9e4
Eric Wong <ewong211@outlook.com>
2019-11-24 01:19:21 -0500

    Revert "added server"
    This reverts commit f62c3f74278133c80c665203d0c68f1c371d4af3.
    
    Revert server in react folder

--- bkexch-react/src/pages/PopUps/SignupPopup.js
+++ bkexch-react/src/pages/PopUps/SignupPopup.js
@@ -29,21 +29,8 @@ class Input extends React.Component {
 	 			return
 		}
 		if (type === "name") {
-    		fetch('/users')
-    		.then((res) => { 
-        		if (res.status === 200) {
-           			return res.json() 
-       			} else {
-            		alert('Could not get users')
-       			}                
-    		})
-    		.then((json) => {  
-        		json.users.filter((u) => u.username === input).length >= 1 ? this.setState({nameMsg: "username exists"}) : this.setState({nameMsg: ""})
-    		}).catch((error) => {
-        		console.log(error)
-    		})
 			 /* Gets users from server and compares it to user email to see if email exist already, requires server call */
-			//users.filter((user) => user.name === input).length >= 1 ? this.setState({nameMsg: "username exists"}) : this.setState({nameMsg: ""})
+			users.filter((user) => user.name === input).length >= 1 ? this.setState({nameMsg: "username exists"}) : this.setState({nameMsg: ""})
 			return
 		}
 		if (type  === "password" || type === "confirmPassword") {
@@ -67,39 +54,20 @@ class Input extends React.Component {
 
 		if (this.state.name !== "" && this.state.nameMsg === "" && this.state.password !== "" && 
 			this.state.passwordMsg === "" && this.state.password === this.state.confirmPassword) {
-			//this.setState({success: "account created"})
-	    	const request = new Request('/signup', {
-        		method: 'post', 
-        		body: JSON.stringify({username: this.state.name, password: this.state.password}),
-        		headers: {
-            		'Accept': 'application/json, text/plain, */*',
-            		'Content-Type': 'application/json'
-        		},
-    		});
+			this.setState({success: "account created"})
 
-			fetch(request)
-			.then(function(res) {
-				if (res.status === 200) {
-					this.setState({success: "account created"})
-
-					this.setState({
-						users: users,
-						name: "",
-						nameMsg: "",
-						password: "",
-						confirmPassword: "",
-						passwordMsg: ""
-					})
-				} else {
-					this.setState({success: "username needs to be min 1 character, password needs to be min 3"})
-				}
-			}.bind(this)).catch((error) => {
-				console.log(error)
-			})
 			/* Gets users from server, requires server call */
-			//const user = {"id": users.length+1, "name": this.state.name, "password": this.state.password, "isAdmin": false}
-			//users.push(user)
+			const user = {"id": users.length+1, "name": this.state.name, "password": this.state.password, "isAdmin": false}
+			users.push(user)
 
+			this.setState({
+				users: users,
+				name: "",
+				nameMsg: "",
+				password: "",
+				confirmPassword: "",
+				passwordMsg: ""
+			})
 		} 
 	}
 
@@ -129,7 +97,7 @@ class Input extends React.Component {
 				 		name = "confirmPassword"
 				 		placeholder = "Confirm Password" /> 
 				 	<p id="passwordMsg"> { this.state.passwordMsg } </p>
-				    {this.state.success !== "account created" ? <p id="success" style={{color: 'red'}}> { this.state.success } </p> :  <p id="success"> { this.state.success } </p> }
+				 	<p id="success"> { this.state.success } </p>
 			</>
 		)
 	}
